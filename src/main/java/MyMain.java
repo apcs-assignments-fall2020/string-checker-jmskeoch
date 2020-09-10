861aa1692fdc4734f7c218c1f88445f90212b7f8
jmskeoch <jmskeoch@yahoo.com>
2020-09-10 02:32:13 +0000

    Implement countABC, containsThe, and isPalindrome method code and respond to user input

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,61 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int count = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if((str.charAt(i) == 'a') || (str.charAt(i) == 'b') || (str.charAt(i) == 'c')) {
+                count++;
+            }
+        }
+        return count;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+
+        return ((str.indexOf("the") >= 0) || (str.indexOf("The") >= 0));
+
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        
+        String str2 = "";
+        for(int i = str.length()-1; i >= 0; i--) {
+            str2 = str2 + str.charAt(i);
+        } 
+
+        return (str.equals(str2));
+
     }
     
     
     public static void main(String[] args) {
+        //Initiate Scanner
         Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
+        //Gather user String input
+        System.out.println("Please enter a string: ");
+        String input = scan.nextLine();
+
+        //Print input's ABC count
+        System.out.println("Your string has " + countABC(input) + " A, B, or C's");
+
+        //Print input's containsThe() boolean
+        if(containsThe(input)) {
+            System.out.println("Your string DOES contain the");
+        } else {
+            System.out.println("Your string DOES NOT contain the");
+        }
+        
+        //Print input's isPalindrome() boolean
+        if(isPalindrome(input)) {
+            System.out.println("Your string IS a palindrome");
+        } else {
+            System.out.println("Your string IS NOT a palindrome");
+        }
+
+        //Close resources
+        scan.close();
     }
 }
